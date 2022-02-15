272e240c22b96dc4595456101bc954cf5ece9f1d
pfranz1 <pfranz1@lsu.edu>
2022-02-15 14:08:16 -0600

    added geters and setters to filter state

--- team_one_application/lib/filter/filterState.dart
+++ team_one_application/lib/filter/filterState.dart
@@ -4,4 +4,22 @@ import 'package:team_one_application/models/friend_ref.dart';
 class FilterState {
   List<FriendRef>? friendRefs;
   FriendListLoadState friendListLoadState = FriendListLoadState.loading;
+
+  set didLoadFriendRefs(List<FriendRef>? loadedFiendRefs) {
+    friendRefs = loadedFiendRefs;
+    friendListLoadState = FriendListLoadState.done;
+  }
+
+  set failedLoadFriendRefs(_) {
+    friendRefs = null;
+    friendListLoadState = FriendListLoadState.error;
+  }
+
+  bool get isLoaded {
+    return friendListLoadState == FriendListLoadState.loading;
+  }
+
+  bool get isError {
+    return friendListLoadState == FriendListLoadState.error;
+  }
 }
