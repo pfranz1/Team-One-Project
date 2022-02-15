b6c77e662eca753bd14ea7af747cbae01ee736d3
pfranz1 <pfranz1@lsu.edu>
2022-02-15 13:53:15 -0600

    FilterView changed to stateless and added internal stateful component

--- team_one_application/lib/filter/filterView.dart
+++ team_one_application/lib/filter/filterView.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:team_one_application/filter/filterController.dart';
 
-class FilterView extends StatefulWidget {
-  const FilterView({Key? key, required this.filterController})
-      : super(key: key);
+class FilterView extends StatelessWidget {
+  FilterView({Key? key, required this.filterController}) : super(key: key);
 
   final FilterController filterController;
 
   @override
-  _FilterViewState createState() => _FilterViewState();
+  Widget build(BuildContext context) {
+    return FilterVisualElement();
+  }
 }
 
-class _FilterViewState extends State<FilterView> {
+class FilterVisualElement extends StatefulWidget {
+  const FilterVisualElement({Key? key}) : super(key: key);
+
+  @override
+  _FilterVisualElementState createState() => _FilterVisualElementState();
+}
+
+class _FilterVisualElementState extends State<FilterVisualElement> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(widget.filterController.filterState.friendRefs?.toString() ??
-          'Friend Refs Loading...'),
+      child: Text('FilterVisualElement'),
     );
   }
 }
