40cff5eca92eca9a3fff43a5db5139d5bd401202
pfranz1 <pfranz1@lsu.edu>
2022-02-15 13:24:25 -0600

    Added a temp friendRef model

---
+++ team_one_application/lib/models/friend_ref.dart
@@ -0,0 +1,7 @@
+// Just a temp friendRef that I (Peter) made so that I can work on the filer. Do whatever you want.
+class FriendRef {
+  String? displayName;
+  String uId;
+
+  FriendRef({required this.displayName, required this.uId});
+}
