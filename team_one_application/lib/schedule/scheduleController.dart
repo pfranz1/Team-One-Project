cbdc7321f34fed130267017536501e706e90d581
pfranz1 <pfranz1@lsu.edu>
2022-02-17 14:44:27 -0600

    Created bare bones of Scheduel module

---
+++ team_one_application/lib/schedule/scheduleController.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:team_one_application/schedule/scheduelState.dart';
+
+class ScheduleController extends ChangeNotifier {
+  ScheduleState scheduleState;
+
+  ScheduleController({required this.scheduleState});
+}
---
+++ team_one_application/lib/schedule/scheduleView.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:team_one_application/schedule/scheduleController.dart';
+
+class ScheduleView extends StatelessWidget {
+  const ScheduleView({Key? key, required this.scheduleController})
+      : super(key: key);
+
+  final ScheduleController scheduleController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('ScheduleView'),
+    );
+  }
+}
